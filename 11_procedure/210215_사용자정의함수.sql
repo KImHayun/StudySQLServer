cacf48a3997b6da08cf33e496a55ad04fa72eebf
KimHayun <95-ha@naver.com>
2021-02-15 17:19:55 +0900

    2월15일

--- 11_procedure/210215_사용자정의함수.sql
+++ 11_procedure/210215_사용자정의함수.sql
@@ -1,7 +1,23 @@
 --»ç¿ëÀÚ Á¤ÀÇ ÇÔ¼ö¸¦ »ç¿ë ¾È ÇÑ °Í--
 select
-*
-(year (getdate())-@birthYear) + 1 as ³ªÀÌ
+*,
+(year(getdate())-@birthYear) + 1 as ³ªÀÌ
+
+	case
+		when (birthYear % 12 = 0) then '¿ø¼şÀÌ'
+		when (birthYear % 12 = 1) then '´ß'
+		when (birthYear % 12 = 2) then '°³'
+		when (birthYear % 12 = 3) then 'µÅÁö'
+		when (birthYear % 12 = 4) then 'Áã'
+		when (birthYear % 12 = 5) then '¼Ò'
+		when (birthYear % 12 = 6) then 'È£¶ûÀÌ'
+		when (birthYear % 12 = 7) then 'Åä³¢'
+		when (birthYear % 12 = 8) then '¿ë'
+		when (birthYear % 12 = 9) then '¹ì'
+		when (birthYear % 12 = 10) then '¸»'
+		else '¾ç'
+
+end as '¶ì'
 
 from userTBL;
 
