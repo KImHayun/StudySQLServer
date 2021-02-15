33b7ad304f62141367ef9309ca2ad82a3780ef66
KimHayun <95-ha@naver.com>
2021-02-15 14:59:45 +0900

    2월15일

--- 07_TSql_adv/210209_tsql_exam2.sql
+++
@@ -1,9 +0,0 @@
--- Union
-select clubName as bName, buildingNum as bLocation from clubtbl
-union
-select stdName, region from stdtbl;
-
--- union all
-select stdName, region from stdtbl
-intersect -- union all
-select stdName, region from stdtbl;
--- 07_TSql_adv/210210_tsql_dynamic_exam.sql
+++
@@ -1,9 +0,0 @@
-use sqlDB;
-go
---exec sp_helpdb;
-declare @sql varchar(100);
-set @sql = 'select * from userTbl where userID = ''EJW''; ';
---print @sql;
-exec(@sql);
-
-select * from userTbl where userID = 'EJW';
