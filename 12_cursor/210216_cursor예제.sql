499bbfea6376402cb62a83623d0e9fb2eb29cd43
KimHayun <95-ha@naver.com>
2021-02-16 10:17:00 +0900

    2월16일

---
+++ 12_cursor/210216_cursor예제.sql
@@ -0,0 +1,32 @@
+use sqlDB;
+go
+
+declare cur_userTbl cursor global 
+	for select height from usertbl;
+
+	open cur_usertbl
+
+	declare @userName nchar(8); --회원이름
+	declare @height smallint; --회원의 키 담는 변수
+	declare @cnt int = 0; --회원수(읽은 행수)
+	declare @totalHeight int = 0; --회원 키의 합계 변수
+	
+	fetch next from cur_usertbl into @height; --커서값 읽어서 @height 변수에 할당
+	
+	close cur_usertbl;
+
+	--반복문 시작
+
+	while @@FETCH_STATUS = 0 
+	begin
+	
+		set @cnt += 1;
+		set @totalHeight += height;
+		fetch next from cur_usertbl into @height;
+	end
+
+	print concat ('회원 키 평균 => ', @totalHeight / @cnt);
+
+
+	close cur_usertbl;
+	deallocate cur_usertbl;
